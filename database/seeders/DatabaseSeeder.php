c72dbcdd73b67a6efa9414b404a4af60036bdc61
Kevin Dubuche <kevindubuche@gmail.com>
2020-12-06 10:49:33 -0500

    setting up caroussel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //   \App\Models\User::factory(10)->create();
-        \App\Models\Livre::factory(50)->create();
+        \App\Models\Livre::factory(10)->create();
     }
 }
