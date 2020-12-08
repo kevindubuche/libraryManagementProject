afd90a453b5daf70da0efa66c70d45f92a4672bc
Kevin Dubuche <kevindubuche@gmail.com>
2020-12-07 20:39:18 -0500

    lite deroulante pout user dans emprunts

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repositories\LivreRepository;
 use App\Models\Livre;
+use App\Models\Emprunt;
 
 class AccueilController extends Controller
 {
@@ -27,6 +28,12 @@ class AccueilController extends Controller
     public function home(Request $request)
     {
         $livres = Livre::all();
+        // $livres_plus_pupulaires = Emprunt::select('*')
+        // ->groupBy('id_livre')
+        // ->orderByRaw('COUNT(*) DESC')
+        // ->limit(3)
+        // ->get();
+        // dd($livres_plus_pupulaires);
         return view('welcome', compact(['livres']));
     }
 
