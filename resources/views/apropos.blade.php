681f42b75faf876a5e66e2ca87cd96016ed83be9
Kevin Dubuche <kevindubuche@gmail.com>
2020-12-07 22:36:26 -0500

    setting up accueil

---
+++ resources/views/apropos.blade.php
@@ -0,0 +1,20 @@
+
+@extends('main')
+@section('apropos') 
+
+
+    <!-- Start Banner -->
+	  <div class="section inner_page_header">
+	     <div class="container">
+		    <div class="row">
+              <div class="col-lg-12">
+			     <div class="full">
+				    <h3>About us</h3>
+				 </div>
+			  </div>
+			</div>
+		 </div>
+	  </div>
+    <!-- end Banner -->
+    
+@endsection
