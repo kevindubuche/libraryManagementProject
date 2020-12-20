4fe6fa8e5be1c3d48561116c1fba79e4c61c7e55
Kevin Dubuche <kevindubuche@gmail.com>
2020-12-20 11:54:42 -0500

    ok

--- resources/views/livres/table.blade.php
+++ resources/views/livres/table.blade.php
@@ -41,8 +41,10 @@
                     {!! Form::open(['route' => ['livres.destroy', $livre->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
                         <a href="{{ route('livres.show', [$livre->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                        @if (Auth::user()->role <= 1)
                         <a href="{{ route('livres.edit', [$livre->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                         {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                        @endif
                     </div>
                     {!! Form::close() !!}
                 </td>
