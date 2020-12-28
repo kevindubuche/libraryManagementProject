f91bdc595e8b485439ba2968c12a619bc671c585
Barbara <bgtheodore18@gmail.com>
2020-12-28 18:09:06 -0500

    MAJ document

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //  \App\Models\User::factory(50)->create();
-           \App\Models\Livre::factory(100)->create();
-         // \App\Models\Emprunt::factory(2)->create();
+         // \App\Models\User::factory(60)->create();
+          // \App\Models\Livre::factory(100)->create();
+          \App\Models\Emprunt::factory(120)->create();
     }
 }
